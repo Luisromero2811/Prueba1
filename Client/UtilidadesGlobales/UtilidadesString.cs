074407b0419e4ec9d03cef8b1df1cd94a527ea0b
Luisromero2811 <19170067@uttcampus.edu.mx>
2023-01-19 10:03:12 -0600

    Cambio1
    Subí primeros cambios

---
+++ Client/UtilidadesGlobales/UtilidadesString.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Prueba1.Client.UtilidadesGlobales
+{
+	public class UtilidadesString
+	{
+          public static string Transformar(string valor) => valor.ToUpper();
+	}
+}
+
---
+++ Shared/Controllers/BlazorPeliculas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Prueba1.Shared.Controllers
+{
+    public class BlazorPeliculas
+    {
+        public string Titulo { get; set; } = null!;
+        public DateTime Fecha_de_lanzamiento { get; set; }
+        public int costo_pelicula { get; set; }
+
+    }
+}
+
